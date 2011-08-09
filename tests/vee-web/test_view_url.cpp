eeee2c555c37768b2ef462de1e36fd396e000cc7
Emmanuel Surleau <esurleau@linagora.com>
2011-08-09 21:36:37 +0200

    - Generates a static lib in vee-web for use when testing - Added basic unit test to vee-web (test View with URLs)

---
+++ tests/vee-web/test_view_url.cpp
@@ -0,0 +1,46 @@
+#include <QObject>
+#include <QtTest/QtTest>
+#include "view.h"
+
+class TestViewUrl: public QObject {
+Q_OBJECT
+private:
+    View* mView;
+    bool mSuccess;
+public slots:
+    void setSuccess(bool success);
+private slots:
+    void init();
+    void testLoadUrlOrPathWithCorrectUrl();
+    void testLoadUrlOrPathWithWrongUrl();
+    void cleanup();
+};
+
+void TestViewUrl::init() {
+    mView = new View();
+    connect(mView, SIGNAL(loadFinished(bool)), this, SLOT(setSuccess(bool)));
+}
+
+void TestViewUrl::testLoadUrlOrPathWithCorrectUrl() {
+    mSuccess = false;
+    mView->loadUrlOrPath(QString("about:blank"));
+    QTest::qWait(10);
+    QCOMPARE(mSuccess, true);
+}
+
+void TestViewUrl::testLoadUrlOrPathWithWrongUrl() {
+    mSuccess = true;
+    mView->loadUrlOrPath(QString("abut:wrongurl"));
+    QTest::qWait(10);
+    QCOMPARE(mSuccess, false);
+}
+
+void TestViewUrl::cleanup() {
+    delete mView;
+}
+
+void TestViewUrl::setSuccess(bool success) {
+    mSuccess = success;
+}
+
+QTEST_MAIN(TestViewUrl)
