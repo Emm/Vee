6b6170e8c792fb07fc7cc490b587c0d969ce1496
Emmanuel Surleau <esurleau@linagora.com>
2011-08-11 22:13:04 +0200

    Moved commandline processing outside of main.cpp in vee-web

--- src/vee-web/main.cpp
+++ src/vee-web/main.cpp
@@ -14,13 +14,10 @@
 #include <QDBusConnection>
 
 #include <cstdlib>
-#include <tclap/CmdLine.h>
 
+#include "commandline.h"
 #include "vee_web_view.h"
 #include "vee_web_view_adaptor_impl.h"
-
-#define PROJECT_NAME "vee-web"
-#define PROJECT_VERSION "0.1"
 
 /**
  Builds HTML from the contents of stdin
@@ -38,32 +35,6 @@ makeHtml() {
     return html;
 }
 
-
-/**
- * Parses the command-line arguments.
- */
-void
-parseArgv(int argc, char** argv, ulong* windowId, std::string& urlOrFile) {
-    try {
-        TCLAP::CmdLine cmd(PROJECT_NAME, ' ', PROJECT_VERSION);
-
-        TCLAP::ValueArg<ulong> winIdArg("w", "window-id", "X server window ID, for embedding", false, 0, "integer");
-        cmd.add(winIdArg);
-
-        TCLAP::UnlabeledValueArg<std::string> urlArg("url", "URL or filename", true, "", "url");
-        cmd.add(urlArg);
-        cmd.parse(argc, argv);
-        if (winIdArg.isSet()) {
-            ulong wid = winIdArg.getValue();
-            *windowId = wid;
-        }
-        urlOrFile.assign(urlArg.getValue());
-    }
-    catch(TCLAP::ArgException &e) {
-        fprintf(stderr, "Error %s for %s\n", e.error().c_str(), e.argId().c_str());
-        std::exit(1);
-    }
-}
 
 void
 exposeVeeWebViewToDBus(VeeWebView* view, ulong instanceId) {
