1c6b1905a1a23135fb34f6f02dbbd1d734c77c72
Emmanuel Surleau <esurleau@linagora.com>
2011-08-17 08:30:26 +0200

    More compact style for test_vee_web_view_file.cpp

--- tests/vee-web/test_vee_web_view_file.cpp
+++ tests/vee-web/test_vee_web_view_file.cpp
@@ -10,59 +10,54 @@
  */
 class TestVeeWebViewFile : public QObject {
 Q_OBJECT
+
 private:
     VeeWebView* mView;
     QTemporaryFile* mHtmlFile;
     bool mSuccess;
 
 public slots:
-    void setSuccess(bool success);
+    void setSuccess(bool success) {
+        mSuccess = success;
+    };
+
 private slots:
-    void init();
-    void cleanup();
-    void testAbsolutePath();
-    void testRelativePath();
+    void init() {
+        // Relative filename (otherwise we can't test relative path), with html
+        // extension (otherwise QWebView won't load the file)
+        mHtmlFile = new QTemporaryFile("test_vee_web_view_file_XXXXXX.HTML");
+        if (mHtmlFile->open()) {
+            QTextStream out(mHtmlFile);
+            out << "<html><body>test</body></html>";
+        }
+        mHtmlFile->close();
+        mView = new VeeWebView();
+        connect(mView, SIGNAL(loadFinished(bool)), this, SLOT(setSuccess(bool)));
+    };
+
+    void cleanup() {
+        delete mView;
+        delete mHtmlFile;
+    };
+
+    void testAbsolutePath() {
+        QFileInfo fileInfo(*mHtmlFile);
+        QString absPath = fileInfo.absoluteFilePath();
+        mSuccess = false;
+        mView->loadUrlOrPath(absPath);
+        QTest::qWait(1000);
+        QCOMPARE(mSuccess, true);
+    };
+
+    void testRelativePath() {
+        QFileInfo fileInfo(*mHtmlFile);
+        QVERIFY(fileInfo.isRelative() == true);
+        QString relPath = fileInfo.filePath();
+        mSuccess = false;
+        mView->loadUrlOrPath(relPath);
+        QTest::qWait(1000);
+        QCOMPARE(mSuccess, true);
+    };
 };
-
-void TestVeeWebViewFile::init() {
-    // Relative filename (otherwise we can't test relative path), with html
-    // extension (otherwise QWebView won't load the file)
-    mHtmlFile = new QTemporaryFile("test_vee_web_view_file_XXXXXX.HTML");
-    if (mHtmlFile->open()) {
-        QTextStream out(mHtmlFile);
-        out << "<html><body>test</body></html>";
-    }
-    mHtmlFile->close();
-    mView = new VeeWebView();
-    connect(mView, SIGNAL(loadFinished(bool)), this, SLOT(setSuccess(bool)));
-}
-
-void TestVeeWebViewFile::testAbsolutePath() {
-    QFileInfo fileInfo(*mHtmlFile);
-    QString absPath = fileInfo.absoluteFilePath(); 
-    mSuccess = false;
-    mView->loadUrlOrPath(absPath);
-    QTest::qWait(1000);
-    QCOMPARE(mSuccess, true);
-}
-
-void TestVeeWebViewFile::testRelativePath() {
-    QFileInfo fileInfo(*mHtmlFile);
-    QVERIFY(fileInfo.isRelative() == true);
-    QString relPath = fileInfo.filePath(); 
-    mSuccess = false;
-    mView->loadUrlOrPath(relPath);
-    QTest::qWait(1000);
-    QCOMPARE(mSuccess, true);
-}
-
-void TestVeeWebViewFile::cleanup() {
-    delete mView;
-    delete mHtmlFile;
-}
-
-void TestVeeWebViewFile::setSuccess(bool success) {
-    mSuccess = success;
-}
 
 QTEST_MAIN(TestVeeWebViewFile)
