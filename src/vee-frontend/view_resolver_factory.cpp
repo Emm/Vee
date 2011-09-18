a0c21d3fa92904fe6b0d8c50ad7676e3b083aa86
Emmanuel Surleau <surleau@idm.fr>
2011-09-18 11:03:54 +0200

    Added -w argument before calling vee-web, otherwise vee-web won't register to dbus

--- src/vee-frontend/view_resolver_factory.cpp
+++ src/vee-frontend/view_resolver_factory.cpp
@@ -9,6 +9,7 @@ ViewResolverFactory::~ViewResolverFactory() {
 ViewResolver* ViewResolverFactory::buildViewResolver() {
     QVector<ViewBuilder*>* viewBuilders = new QVector<ViewBuilder*>();
     EmbedCommand* command = new EmbedCommand(QString("src/vee-web/vee-web"));
+    command->addArgument("-w");
     command->addWinId();
     viewBuilders->append(new ViewBuilder(command,
                 QString("org.vee.VeeWebView"), QString("org.vee.VeeWebView_%1"),
