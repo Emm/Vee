773ebd1d98fa122cca59c6a1610928a807f745ce
Emmanuel Surleau <esurleau@linagora.com>
2011-12-23 23:22:00 +0100

    Added TestViewTab

---
+++ tests/vee-frontend/test_view_tab.cpp
@@ -0,0 +1,41 @@
+#include <QTest>
+#include "view_tab.h"
+#include "dummy_view_builder.h"
+#include "dummy_view.h"
+
+class TestViewTab : public QObject {
+
+Q_OBJECT
+
+private:
+
+    ViewTab* mViewTab;
+
+private slots:
+
+    void init() {
+        QVector<ViewBuilder*>* viewBuilders = new QVector<ViewBuilder*>();
+        viewBuilders->append(new DummyViewBuilder());
+
+        ViewResolver* viewResolver = new ViewResolver(viewBuilders);
+        mViewTab = new ViewTab(new Vim(), viewResolver);
+    }
+
+    void testInitialView() {
+        QVERIFY(mViewTab->view() == NULL);
+    }
+
+    void testSetUrl() {
+        mViewTab->setUrl("dummy url");
+        QVERIFY(mViewTab->view() != NULL);
+        const DummyView* dummyView = dynamic_cast<const DummyView*>(mViewTab->view());
+        QVERIFY(dummyView != NULL);
+    }
+
+    void cleanup() {
+        delete mViewTab;
+    }
+ 
+};
+
+QTEST_MAIN(TestViewTab);
